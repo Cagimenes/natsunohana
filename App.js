60e389e404582c5538a0d03d3517c5b479767fe2
juliamenegassi <julia.menegass159@gmail.com>
2024-03-15 13:53:04 -0300

    criação da home

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './src/Home';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import Home from './src/Home';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={Home} />
+      <Tab.Navigator initialRouteName="Home"
+      screenOptions={{
+        tabBarStyle: { backgroundColor: "#FF994F"}, 
+        headerStyle: { backgroundColor: "#FF994F" , height: 120},
+        tabBarActiveTintColor: "#DD6A00",
+        tabBarInactiveTintColor: "white"
+      }}
+      >
+        <Tab.Screen
+         name="Home" component={Home}
+         options= {{
+          tabBarLabel: "Home",
+          tabBarLabelStyle: { color: "white" },
+          tabBarIcon: ({size }) => (
+            <MaterialCommunityIcons name="home" color="white" size={33} />
+            
+          ),
+          }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,44 @@
-import { Text, View } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Home()
 {
     return(
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+        <Image 
+          style={styles.imagemhome}  
+          source={require('../assets/imagemhome.png')}
+        />
+        <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>FAÇA SEU PEDIDO</Text>
+        </TouchableOpacity>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: 100,
+        alignItems: "center",
+        display: "flex",
+        marginTop: 20,
+        backgroundColor: "#F0E8DE"
+    },
+    imagemhome: {
+        width: "95%",
+        height: 660,
+    },
+    btn: {
+        width: '60%',
+        height: 54,
+        backgroundColor: '#DD6A00',
+        borderRadius: 10,
+        marginTop: -90
+    },
+    btnText: {
+        color: "white",
+        fontSize: 23, 
+        fontWeight: "bold",
+        textAlign: "center",
+        marginTop: 10
+    }
+})
